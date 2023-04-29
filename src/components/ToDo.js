35c0bb52fa53ae8f851477faead258b60994f8da
Rahul <rsc120@nist.edu>
2023-04-29 21:10:14 +0530

    Initial Commit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -115,9 +115,6 @@ const ToDo = () => {
               <span>Check List</span>
             </button>
           </div>
-          <div className="github-link">
-            <p>View the <a href="https://github.com/r6654kumar/ToDo-App-using-React"><i className="fab fa-github"></i> Source Code</a></p>
-          </div>
         </div>
       </div>
     </>
